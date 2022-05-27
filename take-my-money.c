c9fb25cdbeeb765fc6282ba25184ce81df2d5118
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-05-27 12:14:35 -0400

    Update take-my-money.c

--- take-my-money.c
+++ take-my-money.c
@@ -1,15 +1,21 @@
-CURL *curl;
-CURLcode res;
-curl = curl_easy_init();
-if(curl) {
-  curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
-  curl_easy_setopt(curl, CURLOPT_URL, "https://api.apilayer.com/exchangerates_data/convert?to=CLP&from=ARS&amount=879");
-  curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-  curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
-  struct curl_slist *headers = NULL;
-  headers = curl_slist_append(headers, "apikey: 9COfh6HcQz1tPC6M67W17Eqz1wv6r0vq");
-  curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+#include <curl/curl.h>
+#include <stdio.h>
 
-  res = curl_easy_perform(curl);
+int main(void){
+    CURL *curl;
+    CURLcode res;
+    curl = curl_easy_init();
+    if(curl) {
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.apilayer.com/exchangerates_data/convert?to=CLP&from=ARS&amount=879");
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "apikey: 9COfh6HcQz1tPC6M67W17Eqz1wv6r0vq");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+    res = curl_easy_perform(curl);
+    }
+    curl_easy_cleanup(curl);
+    return 0;
 }
-curl_easy_cleanup(curl);
