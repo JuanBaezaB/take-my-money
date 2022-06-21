e57ba8cb5ed1c597fa69e41cf5cd761ec01fc7b0
Juan <51477488+JuanBaezaB@users.noreply.github.com>
2022-06-21 12:42:47 -0400

    Update take-my-money.c

--- take-my-money.c
+++ take-my-money.c
@@ -30,27 +30,6 @@ size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
     return realsize;
 }
 
-int main(void){
-    CURL *curl;
-    CURLcode res;
-    curl = curl_easy_init();
-
-    get_request req = {.buffer = NULL, .len = 0, .buflen = 0};
-
-    if(curl) {
-    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
-    curl_easy_setopt(curl, CURLOPT_URL, "https://api.apilayer.com/exchangerates_data/convert?to=CLP&from=ARS&amount=879");
-    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
-    curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
-    struct curl_slist *headers = NULL;
-    headers = curl_slist_append(headers, "apikey: 9COfh6HcQz1tPC6M67W17Eqz1wv6r0vq");
-    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
-    res = curl_easy_perform(curl);
-    }
-    curl_easy_cleanup(curl);
-    return 0;
-}
 
 int main(int argc, char **argv) {
     CURL *curl;
